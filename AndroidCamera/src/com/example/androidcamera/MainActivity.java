ecb29c2ba78a851f14b3ec337c22b2f815db58b6
hajimemashoo <dinhvantiep2009@gmail.com>
2014-01-03 11:59:46 +0700

    Sửa file MainActivity, có thêm phần TabHost

--- AndroidCamera/src/com/example/androidcamera/MainActivity.java
+++ AndroidCamera/src/com/example/androidcamera/MainActivity.java
@@ -1,23 +1,34 @@
 package com.example.androidcamera;
 
+import android.app.TabActivity;
 import android.os.Bundle;
-import android.app.Activity;
 import android.view.Menu;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
 
-public class MainActivity extends Activity {
+public class MainActivity extends TabActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
+		TabHost tabHost = getTabHost();
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+		TabSpec tabSpec = tabHost.newTabSpec("tab1");
+		tabSpec.setIndicator("Tab 1");
+		tabHost.addTab(tabSpec);
+
+		tabSpec = tabHost.newTabSpec("tab2");
+		tabSpec.setIndicator("Tab 2");
+		tabHost.addTab(tabSpec);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
 }
--- AndroidCamera/src/com/example/androidcamera/MyBrowser.java
+++ AndroidCamera/src/com/example/androidcamera/MyBrowser.java
@@ -1,5 +1,25 @@
 package com.example.androidcamera;
 
-public class MyBrowser {
+import android.app.TabActivity;
+import android.os.Bundle;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
 
+public class MyBrowser extends TabActivity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+
+		TabHost tabHost = getTabHost();
+
+		TabSpec tabSpec = tabHost.newTabSpec("tab1");
+		tabSpec.setIndicator("Tab 1");
+		tabHost.addTab(tabSpec);
+
+		tabSpec = tabHost.newTabSpec("tab2");
+		tabSpec.setIndicator("Tab 2");
+		tabHost.addTab(tabSpec);
+
+	}
 }
